40acb6113464136d96ae4daae3f010f3444e7713
Danielfts <d.trivino@uniandes.edu.co>
2025-05-13 12:03:10 -0500

    complete user

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,1 +1,25 @@
-export class Usuario {}
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import BaseTimestampsEntity from '../../shared/entities/base-timestamps';
+
+@Entity({ name: 'usuarios' })
+export class Usuario extends BaseTimestampsEntity {
+  @Column()
+  numeroCedula: string;
+  @Column()
+  nombre: string;
+  @Column()
+  grupoInvestigacion: string;
+  @Column()
+  numeroExtension: number;
+  @Column()
+  rol: 'Profesor' | 'Decana';
+  @Column({ name: 'id_jefe' })
+  idJefe: bigint;
+
+  @ManyToOne(() => Usuario, (jefe: Usuario) => jefe.empleados)
+  @JoinColumn({ name: 'id_jefe' })
+  jefe: Usuario;
+
+  @OneToMany(() => Usuario, (empleado: Usuario) => empleado.jefe)
+  empleados: Usuario[];
+}
