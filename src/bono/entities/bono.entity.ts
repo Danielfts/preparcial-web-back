5864cdf4d0255821542319a0d7d215f59affb390
Danielfts <d.trivino@uniandes.edu.co>
2025-05-13 12:12:25 -0500

    Completed bono

--- src/bono/entities/bono.entity.ts
+++ src/bono/entities/bono.entity.ts
@@ -1,1 +1,12 @@
-export class Bono {}
+import BaseTimestampsEntity from 'src/shared/entities/base-timestamps';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'bonos' })
+export class Bono extends BaseTimestampsEntity {
+  @Column({ type: 'int2' })
+  monto: number;
+  @Column({ type: 'real' })
+  calificacion: number;
+  @Column()
+  palabraClave: string;
+}
