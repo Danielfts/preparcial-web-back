d3672c250090d657f30412794f3bfc1d616d60c8
Danielfts <d.trivino@uniandes.edu.co>
2025-05-13 13:08:01 -0500

    Complete usuario dto

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUsuarioDto {}
+import { UserType } from '../../shared/types/UserType';
+
+export class CreateUsuarioDto {
+  nombre: string;
+  numeroCedula: number;
+  grupoInvestigacion: string;
+  numeroExtension: number;
+  rol: UserType;
+  idJefe?: bigint;
+}
