fb42cda326c39ad89224edba913587c8502f024b
Danielfts <d.trivino@uniandes.edu.co>
2025-05-13 12:10:05 -0500

    complete classes

--- src/clase/entities/clase.entity.ts
+++ src/clase/entities/clase.entity.ts
@@ -1,1 +1,12 @@
-export class Clase {}
+import BaseTimestampsEntity from 'src/shared/entities/base-timestamps';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'clases' })
+export class Clase extends BaseTimestampsEntity {
+  @Column()
+  nombre: string;
+  @Column()
+  codigo: string;
+  @Column({ type: 'int2' })
+  numeroCreditos: number;
+}
