ba9429792c7290e6d3eb86947bde7386e8cc1c17
Danielfts <d.trivino@uniandes.edu.co>
2025-05-13 11:43:13 -0500

    Added db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import configuration, { config_schema } from './config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration, {
+  config_schema,
+  IConfiguration,
+} from './config/configuration';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<IConfiguration>) => {
+        const db_config: TypeOrmModuleOptions = configService.get('database', {
+          infer: true,
+        })!;
+        return db_config;
+      },
+    }),
     ConfigModule.forRoot({
       validationSchema: config_schema,
       load: [configuration],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,11 +6,13 @@ interface IConfiguration {
   environment: EnvironmentType;
   port: number;
   database: {
+    type: 'postgres';
     host: string;
     port: number;
     username: string;
     password: string;
     database: string;
+    synchronize?: boolean;
   };
 }
 
@@ -18,11 +20,13 @@ export default (): IConfiguration => ({
   environment: (process.env.NODE_ENV as EnvironmentType) || 'development',
   port: parseInt(process.env.PORT!, 10) || 3000,
   database: {
+    type: 'postgres' as const,
     host: process.env.DB_HOST!,
     port: parseInt(process.env.DB_PORT!, 10) || 5432,
     username: process.env.DB_USERNAME!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
+    synchronize: true,
   },
 });
 
